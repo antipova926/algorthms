b5fce82de3b0ca8d7f2868e30a49b431dd9467b5
Anna Antipova <anna.antipova@akvelon.com>
2018-11-29 11:46:17 +0300

    Upload new projects

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+// Declare second integer, double, and String variables.
+int a;
+double b;
+string c;
+// Read and save an integer, double, and String to your variables.
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToDouble(Console.ReadLine());
+c = Console.ReadLine();
+// Print the sum of both integer variables on a new line.
+Console.WriteLine(a + i);
+Console.WriteLine("{0:F1}",d + b);
+Console.WriteLine(s + c);
+
---
+++ PlusMinus/PlusMinus/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr)
+    {
+        int neg = 0;
+        int poz = 0;
+        int z = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0)
+                poz++;
+            if (arr[i] < 0)
+                neg++;
+            if (arr[i] == 0)
+                z++;
+        }
+             
+        Console.WriteLine("{0:F6}", poz / Convert.ToDouble(arr.Length));
+        Console.WriteLine("{0:F6}", neg / Convert.ToDouble(arr.Length));
+        Console.WriteLine("{0:F6}", z / Convert.ToDouble(arr.Length));
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        plusMinus(arr);
+    }
+}
